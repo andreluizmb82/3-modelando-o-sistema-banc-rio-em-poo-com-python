71d55340ec55b0ad02c2d4a33326093bda357f52
Andre Luiz Moura Borges <andreluizmb82@gmail.com>
2024-10-16 22:41:07 -0300

    feat(class Transaction): __str__ implementation

--- bank_system/models/transactions.py
+++ bank_system/models/transactions.py
@@ -1,22 +1,35 @@
-from account import Account
+from datetime import datetime
 
-from abc import ABC, abstractmethod
+RED = "\033[31m"
+GREEN = "\033[32m"
+RESET_COLOR = RC = "\033[0m"
+
+from abc import ABC
+
 class Transaction(ABC):
-    def __init__(self, value: float, date: str) -> None:
-        self.value: float = value
-        self.date: str = date
-        
+    def __init__(self, value: float) -> None:
+        self._value: float = value
+        date = datetime.now()
+        self._date: str = date.strftime("%d/%m/%Y %H:%M")
     
-    @abstractmethod
-    def register(self, account: Account) -> None:
-        pass
+    @property
+    def value(self) -> float:
+        return self._value
+    
+    @property
+    def date(self) -> str:
+        return self._date    
+
 
 class Withdraw(Transaction): 
-    def register(self, account: Account) -> None:
-        # TODO
-        pass
+    def __str__(self) -> str:
+        return RED + f"Saque: \t\tR$ {self.value:.2f} \t {self.date}\n" + RESET_COLOR
+    
 
 class Deposit(Transaction): 
-    def register(self, account: Account) -> None:
-        # TODO
-        pass 
+    def __str__(self) -> str:
+        return GREEN + f"Depósito: \tR$ {self.value:.2f}\t {self.date}\n" + RESET_COLOR
+    
+if __name__ == "__main__":
+    print(Withdraw(100))
+    print(Deposit(100))
