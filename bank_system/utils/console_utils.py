f7c5a2c96d7861cd7e7c932b2c889114674867d3
Andre Luiz Moura Borges <andreluizmb82@gmail.com>
2024-10-23 13:32:04 -0300

    feat(console_utils.py): added color_menu_character()

--- bank_system/utils/console_utils.py
+++ bank_system/utils/console_utils.py
@@ -1,5 +1,17 @@
 import os
 
+RED = "\033[31m"
+YELLOW = Y = "\033[33m"
+GREEN = "\033[32m"
+BLUE = "\033[34m"
+BLUE_BACKGROUND = BB= "\033[44m"
+RESET_COLOR = RC = "\033[0m"
+
+MSG_INVALID = f"{RED}Operação inválida, por favor selecione uma das opções disponíveis no menu.{RESET_COLOR}"
+
+def color_menu_character (letra: str)-> str:
+    return f"{RED}{BB}[{letra}]{RC}"
+
 def clear() -> None:
     if os.name == 'nt':
         os.system('cls')
