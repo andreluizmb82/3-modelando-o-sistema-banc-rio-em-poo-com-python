fcf949a3fc4c22b7e3d1c1023daebd9bee0ccdb3
Andre Luiz Moura Borges <andreluizmb82@gmail.com>
2024-10-23 13:10:03 -0300

    feat(main.py): added web view support
    Modification that adds web view support

--- bank_system/main.py
+++ bank_system/main.py
@@ -1,8 +1,37 @@
-from menu import Menu
+from views.console.menu_console import Menu
+from views.web.menu_html import start_menu_web
+from utils.console_utils import *
 
 def main()-> None:
+    clear()
+    while True:
+        OI = f"{BLUE}Olá, seja bem vindo ao Banco Python!{RESET_COLOR}"
+        MENU = f"""
+{YELLOW}---- MENU ------------------------------------------{RESET_COLOR}
+{color_menu_character('c')}\t Entrar em modo console \t\t   |
+{color_menu_character("w")}\t Entrar em modo web    \t\t\t   |
+{color_menu_character('q')}\t Sair                  \t\t\t   | 
+{OI}\t\t   | 
+{YELLOW}----------------------------------------------------{RESET_COLOR}
+=>"""            
 
-    Menu.start_menu()
+        operation = input(MENU).lower()
+        match operation:
+            case "q":
+                clear()
+                break
+            case "c":
+                clear()
+                Menu.start_menu()
+            case "w":
+                clear()
+                start_menu_web()
+                
+            case _:
+                print(MSG_INVALID)
+ 
+    
+    #Menu.start_menu()
         
 if __name__ == "__main__":
     main()
